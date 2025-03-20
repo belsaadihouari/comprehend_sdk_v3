fc55115ad45d3e53341be21d770251dd3a235e3f
belsaadi houari <stat1401@gmail.com>
2025-03-20 23:39:50 +0100

    e900ee55x

--- entites.js
+++ entites.js
@@ -48,6 +48,11 @@ const input = { // BatchDetectEntitiesRequest
 
 
 
+
+
+  
+
+
   const command = new BatchDetectEntitiesCommand(input);
 const response = await client.send(command);
 const entities = response.ResultList[0].Entities;
