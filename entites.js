c6b28c97a4593aaf42ac123c4d8d0130f45d12bc
belsaadi houari <stat1401@gmail.com>
2025-05-16 20:39:18 +0100

    e900ee55x

--- entites.js
+++ entites.js
@@ -13,18 +13,4 @@ const entities = response.ResultList[0].Entities;
         entities.forEach(entity => {
             console.log(`- Type: ${entity.Type}, Texte: ${entity.Text}, Score: ${entity.Score}`);
         });
-
-
-
-
-
-
-
-
-
-
-
-
-
-     
     
